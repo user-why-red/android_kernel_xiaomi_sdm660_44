6cbf1abf25cc4125a5b74412efa0c8b27891a37f
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-05 15:13:00 -0700

    msm: mdss: Power on display asynchronously as early as possible
    Currently, mdss powers on the display a long time after an unblank is
    requested and gets completely blocked while powering on the display, so if
    the display takes a very long time to turn on, then mdss will be stuck
    unable to do anything else in the meantime. This results in a long delay
    between trying to wake the device and the display actually powering on
    and rendering a frame.
    
    In order to make the display turn on faster when waking the device from
    sleep, start powering on the display as soon as the framebuffer unblank
    event is received. This allows mdss to continue resuming while the display
    takes its time powering on. A high-priority kthread is used here to ensure
    the display powers on as quickly as possible.
    
    To make this work, kickoffs need to be blocked when they attempt to power
    on the display, so that a kickoff won't continue while the display is
    still powered off.
    
    Change-Id: Ieac2f781789a66fa34d04cc51b8783c3fff94092
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- drivers/video/fbdev/msm/mdss_mdp_ctl.c
+++ drivers/video/fbdev/msm/mdss_mdp_ctl.c
@@ -4240,7 +4240,8 @@ static void mdss_mdp_wait_for_panel_on(struct mdss_panel_data *pdata)
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata =
 		container_of(pdata, typeof(*ctrl_pdata), panel_data);
 
-	wait_for_completion(&ctrl_pdata->wake_comp);
+	if (atomic_read(&ctrl_pdata->needs_wake))
+		wait_for_completion(&ctrl_pdata->wake_comp);
 }
 
 int mdss_mdp_ctl_intf_event(struct mdss_mdp_ctl *ctl, int event, void *arg,
