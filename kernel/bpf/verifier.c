0913c5960eca40ac00909c8062e13b1ab4ff9379
Anay Wadhera <awadhera@berkeley.edu>
2021-05-20 21:48:48 -0700

    Revert "bpf/verifier: Add spi variable to check_stack_write()"
    This reverts commit 168cb9b7b2839e861278f9fde03820aba32c4ee0.
    
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -574,7 +574,7 @@ static int check_stack_write(struct verifier_env *env,
 			     struct verifier_state *state, int off,
 			     int size, int value_regno, int insn_idx)
 {
-	int i, spi = (MAX_BPF_STACK + off) / BPF_REG_SIZE;
+	int i;
 	/* caller checked that off % size == 0 and -MAX_BPF_STACK <= off < 0,
 	 * so it's aligned access and [off, off + size) are within stack limits
 	 */
@@ -589,7 +589,8 @@ static int check_stack_write(struct verifier_env *env,
 		}
 
 		/* save register state */
-		state->spilled_regs[spi] = state->regs[value_regno];
+		state->spilled_regs[(MAX_BPF_STACK + off) / BPF_REG_SIZE] =
+			state->regs[value_regno];
 
 		for (i = 0; i < BPF_REG_SIZE; i++) {
 			if (state->stack_slot_type[MAX_BPF_STACK + off + i] == STACK_MISC &&
@@ -619,7 +620,8 @@ static int check_stack_write(struct verifier_env *env,
 		}
 	} else {
 		/* regular write of data into stack */
-		state->spilled_regs[spi] = (struct reg_state) {};
+		state->spilled_regs[(MAX_BPF_STACK + off) / BPF_REG_SIZE] =
+			(struct reg_state) {};
 
 		for (i = 0; i < size; i++)
 			state->stack_slot_type[MAX_BPF_STACK + off + i] = STACK_MISC;
