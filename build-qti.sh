53fd8f714f3d7f899d1266b814fb0347c094979d
TianWalkzzMiku <92043456+TianWalkzzMiku@users.noreply.github.com>
2022-08-20 07:01:33 +0700

    build.sh: script for compile qti

---
+++ build-qti.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+export KERNELNAME=SuperRyzen-CAF-qti
+
+export LOCALVERSION=-V2
+
+export KBUILD_BUILD_USER=Tiannn
+
+export KBUILD_BUILD_HOST=Zyc
+
+export TOOLCHAIN=clang
+
+export DEVICES=whyred,tulip,lavender
+
+source helper
+
+gen_toolchain
+
+send_msg "⏳ Start building ${KERNELNAME} | DEVICES: whyred - tulip - lavender"
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do
+	build ${i} -oldcam
+done
+
+send_msg "⏳ Start building Overclock Version | DEVICES: whyred - tulip"
+
+git apply ./oc.patch
+
+for i in ${DEVICES//,/ }
+do
+	if [ $i == "whyred" ] || [ $i == "tulip" ]
+	then
+		build ${i} -oldcam -overclock
+	fi
+done
+
+END=$(date +"%s")
+
+DIFF=$(( END - START ))
+
+send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
