6ea78d44c50d50d7799d0573dfdb5e96d895e09a
Abel Wu <wuyun.abel@...edance.com>
2023-11-13 03:08:56 +0000

    sock: Fix misuse of sk_under_memory_pressure()
    The status of global socket memory pressure is updated when:
    
      a) __sk_mem_raise_allocated():
    
    	enter: sk_memory_allocated(sk) >  sysctl_mem[1]
    	leave: sk_memory_allocated(sk) <= sysctl_mem[0]
    
      b) __sk_mem_reduce_allocated():
    
    	leave: sk_under_memory_pressure(sk) &&
    		sk_memory_allocated(sk) < sysctl_mem[0]
    
    So the conditions of leaving global pressure are inconstant, which
    may lead to the situation that one pressured net-memcg prevents the
    global pressure from being cleared when there is indeed no global
    pressure, thus the global constrains are still in effect unexpectedly
    on the other sockets.
    
    This patch fixes this by ignoring the net-memcg's pressure when
    deciding whether should leave global memory pressure.
    
    Fixes: e1aab161e013 ("socket: initial cgroup code")
    Signed-off-by: Abel Wu <wuyun.abel@...edance.com>
    
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -2214,7 +2214,7 @@ void __sk_mem_reclaim(struct sock *sk, int amount)
 	sk_memory_allocated_sub(sk, amount);
 	sk->sk_forward_alloc -= amount << SK_MEM_QUANTUM_SHIFT;
 
-	if (sk_under_memory_pressure(sk) &&
+	if (sk_under_global_memory_pressure(sk) &&
 	    (sk_memory_allocated(sk) < sk_prot_mem_limits(sk, 0)))
 		sk_leave_memory_pressure(sk);
 }
