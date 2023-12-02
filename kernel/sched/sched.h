8670f0437f230cd8bf6a6a6a7f7b94c9c09db299
Sultan Alsawaf <sultan@kerneltoast.com>
2023-12-02 06:29:57 +0000

    kernel: Introduce Sbalance IRQ balancer
    This is a simple IRQ balancer that polls every X number of milliseconds and
    moves IRQs from the most interrupt-heavy CPU to the least interrupt-heavy
    CPUs until the heaviest CPU is no longer the heaviest. IRQs are only moved
    from one source CPU to any number of destination CPUs per balance run.
    Balancing is skipped if the gap between the most interrupt-heavy CPU and
    the least interrupt-heavy CPU is below the configured threshold of
    interrupts.
    
    The heaviest IRQs are targeted for migration in order to reduce the number
    of IRQs to migrate. If moving an IRQ would reduce overall balance, then it
    won't be migrated.
    
    The most interrupt-heavy CPU is calculated by scaling the number of new
    interrupts on that CPU to the CPU's current capacity. This way, interrupt
    heaviness takes into account factors such as thermal pressure and time
    spent processing interrupts rather than just the sheer number of them. This
    also makes SBalance aware of CPU asymmetry, where different CPUs can have
    different performance capacities and be proportionally balanced.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -7,6 +7,7 @@
 #include <linux/mutex.h>
 #include <linux/spinlock.h>
 #include <linux/stop_machine.h>
+#include <linux/kthread.h>
 #include <linux/irq_work.h>
 #include <linux/tick.h>
 #include <linux/slab.h>
