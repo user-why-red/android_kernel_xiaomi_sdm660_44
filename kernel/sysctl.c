7e7d99374cad6fb34bd4c1a8e74bd15273fef570
Santhosh <santhosh.user.why.red@gmail.com>
2024-05-24 13:13:00 +0000

    Revert "proc: proc_skip_spaces() shouldn't think it is working on C strings"
    This reverts commit 936345c83f1b16215ca3400bbf2948f2129ace70.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2365,14 +2365,13 @@ int proc_dostring(struct ctl_table *table, int write,
 			       (char __user *)buffer, lenp, ppos);
 }
 
-static void proc_skip_spaces(char **buf, size_t *size)
+static size_t proc_skip_spaces(char **buf)
 {
-	while (*size) {
-		if (!isspace(**buf))
-			break;
-		(*size)--;
-		(*buf)++;
-	}
+	size_t ret;
+	char *tmp = skip_spaces(*buf);
+	ret = tmp - *buf;
+	*buf = tmp;
+	return ret;
 }
 
 static void proc_skip_char(char **buf, size_t *size, const char v)
@@ -2584,7 +2583,7 @@ static int __do_proc_dointvec(void *tbl_data, struct ctl_table *table,
 		bool neg;
 
 		if (write) {
-			proc_skip_spaces(&p, &left);
+			left -= proc_skip_spaces(&kbuf);
 
 			if (!left)
 				break;
@@ -2615,7 +2614,8 @@ static int __do_proc_dointvec(void *tbl_data, struct ctl_table *table,
 	if (!write && !first && left && !err)
 		err = proc_put_char(&buffer, &left, '\n');
 	if (write && !err && left)
-		proc_skip_spaces(&p, &left);
+		left -= proc_skip_spaces(&kbuf);
+free:
 	if (write) {
 		free_page(page);
 		if (first)
@@ -2874,9 +2874,7 @@ static int __do_proc_doulongvec_minmax(void *data, struct ctl_table *table, int 
 		if (write) {
 			bool neg;
 
-			proc_skip_spaces(&p, &left);
-			if (!left)
-				break;
+			left -= proc_skip_spaces(&kbuf);
 
 			err = proc_get_long(&kbuf, &left, &val, &neg,
 					     proc_wspace_sep,
@@ -2907,7 +2905,8 @@ static int __do_proc_doulongvec_minmax(void *data, struct ctl_table *table, int 
 	if (!write && !first && left && !err)
 		err = proc_put_char(&buffer, &left, '\n');
 	if (write && !err)
-		proc_skip_spaces(&p, &left);
+		left -= proc_skip_spaces(&kbuf);
+free:
 	if (write) {
 		free_page(page);
 		if (first)
