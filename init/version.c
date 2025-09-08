257f65485370680f02a3bdf913a7179f5b25105f
Santhosh <santhosh.user.why.red@gmail.com>
2025-09-08 18:41:34 +0530

    init: Spoof kernel version in proc/version
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- init/version.c
+++ init/version.c
@@ -22,15 +22,26 @@ extern int version_string(LINUX_VERSION_CODE);
 int version_string(LINUX_VERSION_CODE);
 #endif
 
+#ifdef CONFIG_SPOOF_KERNEL_VERSION
+#define FIXED_UTS_VERSION CONFIG_SPOOF_KERNEL_VERSION_STRING
+#define FIXED_LINUX_BANNER \
+	"Linux version " UTS_RELEASE " (builder@whyred) " \
+	"(gcc version 4.9.x 20150123 (prerelease)) " FIXED_UTS_VERSION "\n"
+#endif
+
 struct uts_namespace init_uts_ns = {
 	.kref = KREF_INIT(2),
 	.name = {
-		.sysname	= UTS_SYSNAME,
-		.nodename	= UTS_NODENAME,
-		.release	= UTS_RELEASE,
-		.version	= UTS_VERSION,
-		.machine	= UTS_MACHINE,
-		.domainname	= UTS_DOMAINNAME,
+		.sysname    = UTS_SYSNAME,
+		.nodename   = UTS_NODENAME,
+		.release    = UTS_RELEASE,
+#ifdef CONFIG_SPOOF_KERNEL_VERSION
+		.version    = FIXED_UTS_VERSION,
+#else
+		.version    = UTS_VERSION,
+#endif
+		.machine    = UTS_MACHINE,
+		.domainname = UTS_DOMAINNAME,
 	},
 	.user_ns = &init_user_ns,
 	.ns.inum = PROC_UTS_INIT_INO,
@@ -40,12 +51,20 @@ struct uts_namespace init_uts_ns = {
 };
 EXPORT_SYMBOL_GPL(init_uts_ns);
 
+#ifdef CONFIG_SPOOF_KERNEL_VERSION
+const char linux_banner[] = FIXED_LINUX_BANNER;
+#else
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
 	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
 	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+#endif
+
+const char *linux_banner_ptr = linux_banner;
+EXPORT_SYMBOL_GPL(linux_banner_ptr);
 
 const char linux_proc_banner[] =
 	"%s version %s"
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
 	" (" LINUX_COMPILER ") %s\n";
+
