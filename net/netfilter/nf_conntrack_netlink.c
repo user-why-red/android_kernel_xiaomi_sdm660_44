a5936cde4da3645548a3f399e70b6f12b9849061
Hyunwoo Kim <imv4bel@gmail.com>
2026-03-08 02:21:37 +0900

    netfilter: ctnetlink: fix use-after-free in ctnetlink_dump_exp_ct()
    [ Upstream commit 5cb81eeda909dbb2def209dd10636b51549a3f8a ]
    
    ctnetlink_dump_exp_ct() stores a conntrack pointer in cb->data for the
    netlink dump callback ctnetlink_exp_ct_dump_table(), but drops the
    conntrack reference immediately after netlink_dump_start().  When the
    dump spans multiple rounds, the second recvmsg() triggers the dump
    callback which dereferences the now-freed conntrack via nfct_help(ct),
    leading to a use-after-free on ct->ext.
    
    The bug is that the netlink_dump_control has no .start or .done
    callbacks to manage the conntrack reference across dump rounds.  Other
    dump functions in the same file (e.g. ctnetlink_get_conntrack) properly
    use .start/.done callbacks for this purpose.
    
    Fix this by adding .start and .done callbacks that hold and release the
    conntrack reference for the duration of the dump, and move the
    nfct_help() call after the cb->args[0] early-return check in the dump
    callback to avoid dereferencing ct->ext unnecessarily.
    
     BUG: KASAN: slab-use-after-free in ctnetlink_exp_ct_dump_table+0x4f/0x2e0
     Read of size 8 at addr ffff88810597ebf0 by task ctnetlink_poc/133
    
     CPU: 1 UID: 0 PID: 133 Comm: ctnetlink_poc Not tainted 7.0.0-rc2+ #3 PREEMPTLAZY
     Call Trace:
      <TASK>
      ctnetlink_exp_ct_dump_table+0x4f/0x2e0
      netlink_dump+0x333/0x880
      netlink_recvmsg+0x3e2/0x4b0
      ? aa_sk_perm+0x184/0x450
      sock_recvmsg+0xde/0xf0
    
     Allocated by task 133:
      kmem_cache_alloc_noprof+0x134/0x440
      __nf_conntrack_alloc+0xa8/0x2b0
      ctnetlink_create_conntrack+0xa1/0x900
      ctnetlink_new_conntrack+0x3cf/0x7d0
      nfnetlink_rcv_msg+0x48e/0x510
      netlink_rcv_skb+0xc9/0x1f0
      nfnetlink_rcv+0xdb/0x220
      netlink_unicast+0x3ec/0x590
      netlink_sendmsg+0x397/0x690
      __sys_sendmsg+0xf4/0x180
    
     Freed by task 0:
      slab_free_after_rcu_debug+0xad/0x1e0
      rcu_core+0x5c3/0x9c0
    
    Fixes: e844a928431f ("netfilter: ctnetlink: allow to dump expectation per master conntrack")
    Signed-off-by: Hyunwoo Kim <imv4bel@gmail.com>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [uli: backport to 4.19]
    Signed-off-by: Ulrich Hecht <uli@kernel.org>
    [uli: backport to 4.4]
    Signed-off-by: Ulrich Hecht <uli@kernel.org>
    Reviewed-by: Pavel Machek <pavel@nabladev.com>
    Reviewed-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.x90@mail.toshiba>

--- net/netfilter/nf_conntrack_netlink.c
+++ net/netfilter/nf_conntrack_netlink.c
@@ -2726,12 +2726,16 @@ ctnetlink_exp_ct_dump_table(struct sk_buff *skb, struct netlink_callback *cb)
 {
 	struct nfgenmsg *nfmsg = nlmsg_data(cb->nlh);
 	struct nf_conn *ct = cb->data;
-	struct nf_conn_help *help = nfct_help(ct);
+	struct nf_conn_help *help;
 	u_int8_t l3proto = nfmsg->nfgen_family;
 	unsigned long last_id = cb->args[1];
 	struct nf_conntrack_expect *exp;
 
 	if (cb->args[0])
+		return 0;
+
+	help = nfct_help(ct);
+	if (!help)
 		return 0;
 
 	rcu_read_lock();
@@ -2763,6 +2767,24 @@ out:
 	return skb->len;
 }
 
+static int ctnetlink_dump_exp_ct_start(struct netlink_callback *cb)
+{
+	struct nf_conn *ct = cb->data;
+
+	if (!atomic_inc_not_zero(&ct->ct_general.use))
+		return -ENOENT;
+	return 0;
+}
+
+static int ctnetlink_dump_exp_ct_done(struct netlink_callback *cb)
+{
+	struct nf_conn *ct = cb->data;
+
+	if (ct)
+		nf_ct_put(ct);
+	return 0;
+}
+
 static int ctnetlink_dump_exp_ct(struct sock *ctnl, struct sk_buff *skb,
 				 const struct nlmsghdr *nlh,
 				 const struct nlattr * const cda[])
@@ -2777,6 +2799,8 @@ static int ctnetlink_dump_exp_ct(struct sock *ctnl, struct sk_buff *skb,
 	struct nf_conntrack_zone zone;
 	struct netlink_dump_control c = {
 		.dump = ctnetlink_exp_ct_dump_table,
+		.start = ctnetlink_dump_exp_ct_start,
+		.done = ctnetlink_dump_exp_ct_done,
 	};
 
 	err = ctnetlink_parse_tuple(cda, &tuple, CTA_EXPECT_MASTER,
