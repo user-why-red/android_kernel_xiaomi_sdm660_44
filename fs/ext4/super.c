62d717f589023e1f2c15f432200c93e965692301
Helen Koike <koike@igalia.com>
2026-03-17 11:23:10 -0300

    ext4: reject mount if bigalloc with s_first_data_block != 0
    commit 3822743dc20386d9897e999dbb990befa3a5b3f8 upstream.
    
    bigalloc with s_first_data_block != 0 is not supported, reject mounting
    it.
    
    Signed-off-by: Helen Koike <koike@igalia.com>
    Suggested-by: Theodore Ts'o <tytso@mit.edu>
    Reported-by: syzbot+b73703b873a33d8eb8f6@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=b73703b873a33d8eb8f6
    Link: https://patch.msgid.link/20260317142325.135074-1-koike@igalia.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Cc: stable@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -2626,6 +2626,13 @@ static int ext4_feature_set_ok(struct super_block *sb, int readonly)
 			 "extents feature\n");
 		return 0;
 	}
+	if (ext4_has_feature_bigalloc(sb) &&
+	    le32_to_cpu(EXT4_SB(sb)->s_es->s_first_data_block)) {
+		ext4_msg(sb, KERN_WARNING,
+			 "bad geometry: bigalloc file system with non-zero "
+			 "first_data_block\n");
+		return 0;
+	}
 
 #ifndef CONFIG_QUOTA
 	if (ext4_has_feature_quota(sb) && !readonly) {
